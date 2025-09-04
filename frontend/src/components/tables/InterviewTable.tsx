9479bf47dc0d959f6f86760d4408f17cf36bd50c
Emmanuel <emmanuel.pean@gmail.com>
2025-09-04 21:34:03 +0200

    Refactor `InterviewsTable`: migrate to TypeScript, update to use `useProvidedTableData`, and enhance type safety with new interfaces.

---
+++ frontend/src/components/tables/InterviewTable.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { GenericTableWithModals, useProvidedTableData } from "./GenericTable";
+import { columns } from "../rendering/view/TableColumnRenders";
+import { InterviewModal } from "../modals/InterviewModal";
+
+interface InterviewsTableProps {
+	jobApplicationId: number;
+	onChange?: () => void;
+	data?: any[] | null;
+}
+
+interface Interview {
+	id: number;
+	date: string;
+	type: string;
+	location: string;
+	note: string;
+	job_application_id: number;
+	// Add other interview properties as needed
+}
+
+interface ModalProps {
+	submode: "add" | "edit" | "view";
+	onSuccess?: (item: Interview) => void;
+}
+
+const InterviewsTable: React.FC<InterviewsTableProps> = ({ jobApplicationId, onChange, data = null }) => {
+	const {
+		data: interviewData,
+		loading,
+		error,
+		addItem,
+		sortConfig,
+		setSortConfig,
+		updateItem,
+		deleteItem,
+	} = useProvidedTableData(data, { key: "date", direction: "desc" });
+
+	const ViewColumns = [columns.date!(), columns.type!(), columns.location!(), columns.note!()];
+
+	const ModalWithProps: React.FC<ModalProps> = (
+		props, // @ts-ignore // TODO
+	) => <InterviewModal {...props} jobApplicationId={jobApplicationId} />;
+
+	return (
+		<GenericTableWithModals
+			data={interviewData}
+			columns={ViewColumns}
+			sortConfig={sortConfig}
+			onSort={setSortConfig}
+			loading={loading}
+			error={error}
+			Modal={ModalWithProps}
+			endpoint="interviews"
+			nameKey="date"
+			itemType="Interview"
+			addItem={addItem}
+			updateItem={updateItem}
+			removeItem={deleteItem}
+			setData={() => {}}
+			ModalSize="lg"
+			showAllEntries={true}
+			compact={true}
+		/>
+	);
+};
+
+export default InterviewsTable;
