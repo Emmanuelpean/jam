6d27fae485ad899498e0a3ee4f920141798d08dd
Emmanuel Pean <emmanuel.pean@gmail.com>
2025-12-18 22:59:48 +0000

    Fixed the job scraping service log issue causing it to initialise multiple times [skip tests]

--- backend/app/job_email_scraping/email_scraper.py
+++ backend/app/job_email_scraping/email_scraper.py
@@ -7,10 +7,12 @@ content), and records run statistics in an EisServiceLog."""
 
 import traceback
 from datetime import datetime
+from functools import lru_cache
 
 from app import utils, model_registry as models
 from app.config import settings
 from app.database import get_db
+from app.emails.email_service import EmailService
 from app.job_email_scraping.email_parsers import JOB_PARSERS, ALERT_NAME_EXTRACTORS, PLATFORM_SENDER_EMAILS
 from app.job_email_scraping.email_parsers.utils import Platform, remove_style_tags
 from app.job_email_scraping.job_scrapers import JobResult
@@ -26,7 +28,6 @@ from app.job_email_scraping.models import (
     JobEmailScrapingPlatformStat,
     JobEmailScrapingServiceError,
 )
-from app.emails.email_service import EmailService
 from app.service_runner import ServiceRunner
 from app.utils import AppLogger
 
@@ -547,7 +548,7 @@ class JobEmailScraper(EmailService):
                     self.upsert_platform_stat(service_log, job_record.platform, job_scrape_failed_ids=job_record.id)
 
 
-class EmailScraperService(ServiceRunner):
+class EmailScraperServiceRunner(ServiceRunner):
     """Service runner for the EmailScraperService"""
 
     def __init__(self) -> None:
@@ -556,4 +557,8 @@ class EmailScraperService(ServiceRunner):
         ServiceRunner.__init__(self, SERVICE_NAME, dict(timedelta_days=3), JobEmailScraper().run_scraping)
 
 
-scraper_service = EmailScraperService()
+@lru_cache()
+def get_email_scraper_service() -> EmailScraperServiceRunner:
+    """Singleton per worker process"""
+
+    return EmailScraperServiceRunner()
