9387a05e24099621ea5ec19fdbbbe59196afc62f
Emmanuel <emmanuel.pean@gmail.com>
2026-01-30 09:10:16 +0000

    Added missing vars

--- backend/app/config.py
+++ backend/app/config.py
@@ -77,7 +77,7 @@ class Settings(BaseSettings):
     stripe_secret_key: str
     stripe_webhook_secret: str
     stripe_toast_price_id: str
-    stripe_publishable_key: str
+    # stripe_publishable_key: str
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env",
