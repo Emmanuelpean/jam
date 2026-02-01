7228fee4fb332867ff8cc5f1836cd2d38fd96350
Emmanuel <emmanuel.pean@gmail.com>
2026-02-01 22:39:08 +0000

    Removed headless

--- frontend/tests/conftest.py
+++ frontend/tests/conftest.py
@@ -1958,7 +1958,7 @@ class BaseTest(BaseUtils):
                 "protocol_handler": {"excluded_schemes": {"mailto": True}},
             }
             chrome_options.add_experimental_option("prefs", prefs)
-            # chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--headless=new")
             chrome_options.add_argument("--window-size=1960,1080")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--no-sandbox")
