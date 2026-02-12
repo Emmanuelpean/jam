e92dfced48197b9d94197ea851ceab11ad68ef03
Emmanuel Pean <emmanuel.pean@gmail.com>
2026-02-12 22:41:57 +0000

    Merged with develop [skip tests]

--- frontend/src/services/schemas/Core.ts
+++ frontend/src/services/schemas/Core.ts
@@ -28,6 +28,7 @@ export interface UserPreferences {
 	update_limit: number;
 	default_currency: string;
 	extension_banner_dismissed: boolean;
+	dashboard_layout: string | null;
 	table_columns: Record<string, string[]> | null;
 	table_sort: Record<string, { key: string; direction: string }> | null;
 }
