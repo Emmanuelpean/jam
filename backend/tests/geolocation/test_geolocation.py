ce39126ecaa51a66e1ea143599959d4b26d97814
Emmanuel Pean <emmanuel.pean@gmail.com>
2026-02-20 09:16:29 +0000

    Chrome extension improvements [skip tests]

--- backend/tests/geolocation/test_geolocation.py
+++ backend/tests/geolocation/test_geolocation.py
@@ -6,7 +6,7 @@ import time
 import pytest
 import requests
 
-import app.geolocation as geolocation_module
+import app.geolocation.geolocation as geolocation_module
 from app.geolocation.geolocation import call_geocoding_api, geocode_location
 from app.models import Geolocation
 from tests.utils.create_data.utils import create_db_entries
